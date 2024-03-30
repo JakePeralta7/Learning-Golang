224eea494fc85898971685da698dfc63070625ec
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-03-30 13:30:18 +0300

    Create isProcessRunning.go

---
+++ isProcessRunning/isProcessRunning.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/process"
+)
+
+func isProcessRunning(processName string) (bool, error) {
+	processes, err := process.Processes()
+	if err != nil {
+		return false, err
+	}
+
+	for _, p := range processes {
+		name, err := p.Name()
+		if err != nil {
+			continue
+		}
+		if name == processName {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func main() {
+	processName := "notepad.exe"
+	isRunning, err := isProcessRunning(processName)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	if isRunning {
+		fmt.Printf("The process %s is running.\n", processName)
+	} else {
+		fmt.Printf("The process %s is not running.\n", processName)
+	}
+}
