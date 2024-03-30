e8db88d0689b2eff8b6fb377705ea327113a8922
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-03-30 13:43:33 +0300

    Update isProcessRunning.go

--- isProcessRunning/isProcessRunning.go
+++ isProcessRunning/isProcessRunning.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/shirou/gopsutil/process"
 )
 
@@ -25,16 +26,22 @@ func isProcessRunning(processName string) (bool, error) {
 }
 
 func main() {
-	processName := "notepad.exe"
-	isRunning, err := isProcessRunning(processName)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	}
+	processList := []string{
+		"notepad.exe",
+		"explorer.exe",
+		"chrome.exe"} // replace with your list of process names
 
-	if isRunning {
-		fmt.Printf("The process %s is running.\n", processName)
-	} else {
-		fmt.Printf("The process %s is not running.\n", processName)
+	for _, processName := range processList {
+		isRunning, err := isProcessRunning(processName)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
+		}
+
+		if isRunning {
+			fmt.Printf("The process %s is running.\n", processName)
+		} else {
+			fmt.Printf("The process %s is not running.\n", processName)
+		}
 	}
 }
